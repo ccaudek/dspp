7c4a9cd9cd73374692661f15134908bd45e4707b
Corrado Caudek <corrado.caudek@unifi.it>
2021-09-23 08:55:26 +0200

    025 20210923

--- _common.R
+++ _common.R
@@ -1,18 +1,3 @@
-# example R options set globally
-options(width = 60)
-
-# example chunk options set globally
-knitr::opts_chunk$set(
-  comment = "#>",
-  collapse = TRUE,
-  message = FALSE, 
-  warning = FALSE,
-  error = FALSE,
-  fig.height = 3, 
-  fig.width = 6,
-  fig.align = "center"
-  )
-
 suppressPackageStartupMessages(library("here"))
 suppressPackageStartupMessages(library("tidyverse"))
 suppressPackageStartupMessages(library("patchwork"))
@@ -21,6 +6,9 @@ suppressPackageStartupMessages(library("ggExtra"))
 suppressPackageStartupMessages(library("ggpubr"))
 suppressPackageStartupMessages(library("khroma"))
 
+# R options set globally
+options(width = 60)
+set.seed(42)
 SEED <- 374237 # set random seed for reproducibility
 # theme_set(bayesplot::theme_default(base_family = "sans"))
 theme_set(bayesplot::theme_default())
@@ -28,5 +16,19 @@ theme_set(bayesplot::theme_default())
 colors <- colour("bright", names = FALSE)(4)
 ggplot <- function(...) ggplot2::ggplot(...) + scale_color_bright()
 theme_update(plot.title = element_text(hjust = 0.5))
+
+# chunk options set globally
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  message = FALSE, 
+  warning = FALSE,
+  error = FALSE,
+  tidy = "styler",
+  fig.height = 3, 
+  fig.width = 6,
+  fig.align = "center",
+  knitr::opts_chunk$set(cache.extra = knitr::rand_seed)
+  )
 
 
